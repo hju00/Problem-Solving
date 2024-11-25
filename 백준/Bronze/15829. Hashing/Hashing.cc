a0113d06b42bc7ca4c438eb196a8ff84ca198319
Park HyeongJu <113781201+hju00@users.noreply.github.com>
2024-11-25 14:52:48 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -4,22 +4,22 @@
 using namespace std;
 
 int main() {
-    
     int L;
     string s;
-    long long ans = 0;
+    long long ans = 0;       // 해시 값을 저장할 변수
+    long long r_power = 1;   // r^i 값을 계산하며 모듈로 연산 유지
+
     cin >> L;
     cin >> s;
-    int r_power = 1;
 
-    for(int i = 0; i < L; i++){
-        long long temp = ((s[i] - 'a' + 1) * r_power) % M;
-        r_power *= r;
-        r_power %= M;
-        ans += temp;
+    for (int i = 0; i < L; i++) {
+        // 현재 문자의 해시 값을 계산하고 누적
+        ans = (ans + (s[i] - 'a' + 1) * r_power % M) % M;
+
+        // r^i 값을 갱신하며 모듈로 연산 적용
+        r_power = (r_power * r) % M;
     }
 
     cout << ans << endl;
-
     return 0;
 }
